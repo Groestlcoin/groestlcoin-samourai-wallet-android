365f134c8f78df86ef3dda28547657b479fb9c55
HashEngineering <hashengineeringsolutions@gmail.com>
2019-01-05 22:32:52 -0800

    Add Groestlcoin to email subject

--- app/src/main/java/com/samourai/wallet/BalanceActivity.java
+++ app/src/main/java/com/samourai/wallet/BalanceActivity.java
@@ -1177,7 +1177,7 @@ public class BalanceActivity extends Activity {
                                         Toast.makeText(BalanceActivity.this, R.string.copied_to_clipboard, Toast.LENGTH_SHORT).show();
                                     } else {
                                         Intent email = new Intent(Intent.ACTION_SEND);
-                                        email.putExtra(Intent.EXTRA_SUBJECT, "Samourai Wallet backup");
+                                        email.putExtra(Intent.EXTRA_SUBJECT, "Groestlcoin Samourai Wallet backup");
                                         email.putExtra(Intent.EXTRA_TEXT, obj.toString());
                                         email.setType("message/rfc822");
                                         startActivity(Intent.createChooser(email, BalanceActivity.this.getText(R.string.choose_email_client)));
