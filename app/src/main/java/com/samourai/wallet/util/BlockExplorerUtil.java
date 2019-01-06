ddf14267d82e0da6100da0518148f306742115ba
HashEngineering <hashengineeringsolutions@gmail.com>
2019-01-05 22:15:10 -0800

    Update testnet block explorers

--- app/src/main/java/com/samourai/wallet/util/BlockExplorerUtil.java
+++ app/src/main/java/com/samourai/wallet/util/BlockExplorerUtil.java
@@ -6,11 +6,11 @@ public class BlockExplorerUtil {
 
     private static CharSequence[] blockExplorers = { "Chainz", "Groestlsight"};
     private static CharSequence[] blockExplorerTxUrls = { "https://chainz.cryptoid.info/grs/tx.dws?", "https://groestlsight.groestlcoin.org/tx/"};
-    private static CharSequence[] blockExplorerAddressUrls = { "https://www.smartbit.com.au/address/", "http://srv1.yogh.io/#addr:id:", "https://live.blockcypher.com/btc/address/", "https://live.blockcypher.com/btc/address/" };
+    private static CharSequence[] blockExplorerAddressUrls = { "https://chainz.cryptoid.info/grs/block.dws?", "https://groestlsight.groestlcoin.org/block/" };
 
     private static CharSequence[] tBlockExplorers = { "Chainz", "Groestlsight"};
-    private static CharSequence[] tBlockExplorerTxUrls = { "https://testnet.smartbit.com.au/tx/", "https://live.blockcypher.com/btc-testnet/tx/" };
-    private static CharSequence[] tBlockExplorerAddressUrls = { "https://testnet.smartbit.com.au/address/", "https://live.blockcypher.com/btc-testnet/address/" };
+    private static CharSequence[] tBlockExplorerTxUrls = { "https://chainz.cryptoid.info/grs-test/tx.dws?", "https://groestlsight-test.groestlcoin.org/tx/" };
+    private static CharSequence[] tBlockExplorerAddressUrls = { "https://chainz.cryptoid.info/grs-test/block.dws?", "https://groestlsight-test.groestlcoin.org/block/" };
 
     private static BlockExplorerUtil instance = null;
 
