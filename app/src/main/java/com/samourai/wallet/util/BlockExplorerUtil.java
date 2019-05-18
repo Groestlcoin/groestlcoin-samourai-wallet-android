ed1de18dda8df7a5e1bff8f978962aa64c101a5f
HashEngineering <hashengineeringsolutions@gmail.com>
2019-05-18 10:16:43 -0700

    Add blockbook block explorer to testnet list

--- app/src/main/java/com/samourai/wallet/util/BlockExplorerUtil.java
+++ app/src/main/java/com/samourai/wallet/util/BlockExplorerUtil.java
@@ -8,7 +8,7 @@ public class BlockExplorerUtil {
     private static CharSequence[] blockExplorerTxUrls = { "https://chainz.cryptoid.info/grs/tx.dws?", "https://groestlsight.groestlcoin.org/tx/", "https://blockbook.groestlcoin.org/tx/"};
     private static CharSequence[] blockExplorerAddressUrls = { "https://chainz.cryptoid.info/grs/address.dws?", "https://groestlsight.groestlcoin.org/address/", "https://blockbook.groestlcoin.org/address/" };
 
-    private static CharSequence[] tBlockExplorers = { "Chainz", "Groestlsight"};
+    private static CharSequence[] tBlockExplorers = { "Chainz", "Groestlsight", "Blockbook"};
     private static CharSequence[] tBlockExplorerTxUrls = { "https://chainz.cryptoid.info/grs-test/tx.dws?", "https://groestlsight-test.groestlcoin.org/tx/", "https://blockbook-test.groestlcoin.org/tx/" };
     private static CharSequence[] tBlockExplorerAddressUrls = { "https://chainz.cryptoid.info/grs-test/address.dws?", "https://groestlsight-test.groestlcoin.org/address/", "https://blockbook-test.groestlcoin.org/address/" };
 
