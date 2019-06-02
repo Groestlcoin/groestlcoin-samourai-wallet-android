883f824251b8216307424e6af62eba51f295c22a
HashEngineering <hashengineeringsolutions@gmail.com>
2019-06-01 19:23:20 -0700

    Set testnet notification tx fee address

--- app/src/main/java/com/samourai/wallet/bip47/SendNotifTxFactory.java
+++ app/src/main/java/com/samourai/wallet/bip47/SendNotifTxFactory.java
@@ -11,7 +11,7 @@ public class SendNotifTxFactory	{
 //    public static final BigInteger _bSWCeilingFee = BigInteger.valueOf(50000L);
 
     public static final String SAMOURAI_NOTIF_TX_FEE_ADDRESS = "3K8eqP6j14JzPnmRMG6exTsNo8iUZHEH5e";  //GRS
-    public static final String TESTNET_SAMOURAI_NOTIF_TX_FEE_ADDRESS = "tb1qh287jqsh6mkpqmd8euumyfam00fkr78qhrdnde";
+    public static final String TESTNET_SAMOURAI_NOTIF_TX_FEE_ADDRESS = "tgrs1qsetg4dslqsytuj7xj7k6h7lnkp52qmpy0rgyra";
 
 //    public static final double _dSWFeeUSD = 0.5;
 
